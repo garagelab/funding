d7673c84f357440c18ad72dac473a1848f0347de
Gonzalo Bellver <gonzalo.bellver@gmail.com>
2013-01-03 18:36:27 -0300

    Minor fixes

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -19,13 +19,20 @@ var App = function App() {
         self.ftClient = new FTClient('1vAIR_rP8fzPs_EYiaKEgXSq8yNa2Z1yV12SnOXI');
 
         self.ftClient.query({
-            select: ['provincias-en-las-que-actualmente-se-aplica-el-instrumento', 'beneficiarios', 'mecanismos', 'tema', 'titulo'],
-            limit: 20
+            select: ['provincias-en-las-que-actualmente-se-aplica-el-instrumento', 'beneficiarios', 'mecanismos', 'tema',
+                'titulo', 'detalle-y-cantidad-de-beneficiarios', 'objetivos', 'beneficios', 'monto', 'web', 'area', 'organismo', 'telefono']
         }, function(data) {
             data.table.rows.map(self.addItem, self);
             var item_template =
                 '<div class="item">' +
                     '<h4><%= obj.title %></h4>' +
+                    '<p><strong>Detalle: </strong><%= obj.details %></p>' +
+                    '<p><strong>Objetivos: </strong><%= obj.objectives %></p>' +
+                    '<p><strong>Beneficios: </strong><%= obj.benefits %></p>' +
+                    '<p><strong>Monto: </strong><%= obj.amount %></p>' +
+                    '<p><strong>Fechas: </strong> TODO: add</p>' +
+                    '<p><strong>Web: </strong><a href="<%= obj.web %>" target="_blank"><%= obj.web %></a></p>' +
+                    '<p><strong>Contactos: </strong><%= obj.phone %> / <%= obj.area %> / <%= obj.entity %></p>'
                     '</div>';
 
             $.facetelize({
@@ -39,9 +46,12 @@ var App = function App() {
                 resultSelector   : '#results',
                 facetSelector    : '#facets',
                 resultTemplate   : item_template,
-                paginationCount  : 50
-//                orderByOptions   : {'firstname': 'First name', 'lastname': 'Last name', 'category': 'Category', 'RANDOM': 'Random'},
-//                facetSortOption  : {'continent': ["North America", "South America"]}
+                paginationCount  : 10,
+                orderByTemplate  : '',
+                countTemplate    : '<div class=facettotalcount><%= count %> resultados</div>',
+                deselectTemplate : '<div class=deselectstartover>Limpiar filtro</div>',
+                showMoreTemplate : '<a id=showmorebutton>Más resultados</a>',
+                noResults        : '<div class=results>No hay ningún resultado para esta búsqueda</div>'
             });
 
         })
@@ -54,7 +64,15 @@ var App = function App() {
             'beneficiaries' : row[1].split(', '),
             'mecanisms' : row[2].split(', '),
             'topics' : row[3].split(', '),
-            'title' : row[4]
+            'title' : row[4],
+            'details' : row[5],
+            'objectives' : row[6],
+            'benefits' : row[7],
+            'amount' : row[8],
+            'web' : row[9],
+            'area' : row[10],
+            'entity' : row[11],
+            'phone' : row[12]
         });
     }
 }
