0a8db172ba765745d7880ec4a0fdfe508b591a7c
Gonzalo Bellver <gonzalo.bellver@gmail.com>
2013-05-03 17:24:32 -0300

    Adapted for new data

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -15,41 +15,62 @@ var App = function App() {
     this.init = function() {
         var self = this;
 
-        self.ftClient = new FTClient('1vAIR_rP8fzPs_EYiaKEgXSq8yNa2Z1yV12SnOXI');
+        self.ftClient = new FTClient('1eb4iyrFgENme011jHDwM44P1KWUwxRfKrPAC5V8');
 
         self.ftClient.query({
-            select: ['provincias-en-las-que-actualmente-se-aplica-el-instrumento', 'beneficiarios', 'mecanismos',
-                'tema', 'titulo', 'detalle-y-cantidad-de-beneficiarios', 'objetivos', 'beneficios', 'monto', 'web',
-                'area', 'organismo', 'telefono', 'fecha-de-inicio', 'fecha-de-cierre']
+            select: [
+                "fondo",
+                "provincias_de_aplicacion",
+                "beneficiarios",
+                "mecanismos",
+                "sector",
+                "fase_de_desarrollo",
+                "tipo_de_proyecto",
+                "emp_etapas",
+                "ins_etapas",
+                "emp_areas",
+                "ins_areas",
+                "tipo_de_convocatoria",
+                "beneficiarios_descripcion",
+                "beneficiarios_tipo",
+                "objetivos",
+                "tipo_de_financiamiento",
+                "monto_maximo",
+                "cobertura_maxima",
+                "plazo_max",
+                "detalle"
+            ]
         }, function(data) {
             data.rows.map(self.addItem, self);
+
+            var nonEmptyString = function(str) {
+                return ;
+            };
+
             var item_template =
                 '<div class="item">' +
-                    '<h3><%= obj.title %></h3>' +
-                    '<p><strong>Detalle: </strong><%= obj.details %></p>' +
-                    '<p><strong>Objetivos: </strong><%= obj.objectives %></p>' +
-                    '<p><strong>Beneficios: </strong><%= obj.benefits %></p>' +
-                    '<p><strong>Monto: </strong><%= obj.amount %></p>' +
-                    '<% if (obj.startDate || obj.endDate) { %>' +
-                    '<p><strong>Fechas: </strong>' +
-                    '<% if (obj.startDate) { %>desde el <%= obj.startDate %> <% } %><% if (obj.endDate) { %>desde el <%= obj.endDate %> <% } %>' +
-                    '</p><% } %>' +
-                    '<p><strong>Web: </strong><a href="<%= obj.web %>" target="_blank"><%= obj.web %></a></p>' +
-                    '<p><strong>Contactos: </strong>' +
-                    '<%= obj.phone %> ' +
-                    '<% if (obj.area != "Indefinido" && obj.area != "OTRA Secretaría") { %> / <%= obj.area %> <% } %>' +
-                    '<% if (obj.entity != "OTRO Ministerio") { %> / <%= obj.entity %> <% } %></p>' +
+                    '<h3><%= fondo %></h3>' +
+                    '<p><strong>Detalle: </strong><%= (detalle == "" || detalle == null) ? "-" : detalle %></p>' +
+                    '<p><strong>Sector: </strong><%= (sector == "" || sector == null) ? "-" : sector %></p>' +
+                    '<p><strong>Fase de desarrollo: </strong><%= (fase_de_desarrollo == "" || fase_de_desarrollo == null) ? "-" : fase_de_desarrollo %></p>' +
+                    '<p><strong>Tipo de proyecto: </strong><%= (tipo_de_proyecto == "" || tipo_de_proyecto == null) ? "-" : tipo_de_proyecto %></p>' +
+                    '<p><strong>Tipo de convocatoria: </strong><%= (tipo_de_convocatoria == "" || tipo_de_convocatoria == null) ? "-" : tipo_de_convocatoria %></p>' +
+                    '<p><strong>Descripción de beneficiarios: </strong><%= (beneficiarios_descripcion == "" || beneficiarios_descripcion == null) ? "-" : beneficiarios_descripcion %></p>' +
+                    '<p><strong>Tipo de beneficiarios: </strong><%= (beneficiarios_tipo == "" || beneficiarios_tipo == null) ? "-" : beneficiarios_tipo %></p>' +
+                    '<p><strong>Objetivos: </strong><%= (objetivos == "" || objetivos == null) ? "-" : objetivos %></p>' +
+                    '<p><strong>Tipo de financiamiento: </strong><%= (tipo_de_financiamiento == "" || tipo_de_financiamiento == null) ? "-" : tipo_de_financiamiento %></p>' +
+                    '<p><strong>Monto máximo: </strong><%= (monto_maximo == "" || monto_maximo == null) ? "-" : "$" + monto_maximo %></p>' +
+                    '<p><strong>Cobertura máxima: </strong><%= (cobertura_maxima == "" || cobertura_maxima == null) ? "-" : cobertura_maxima %></p>' +
+                    '<p><strong>Plazo máximo: </strong><%= (plazo_max == "" || plazo_max == null) ? "-" : plazo_max %></p>' +
                     '<hr/>' +
                     '</div>';
 
             $.facetelize({
                 items            : self.items,
                 facets           : {
-                    'provinces' : 'Provincias',
-                    'beneficiaries' : 'Beneficiarios',
-                    'mecanisms' : 'Mecanismos',
-                    'topics' : 'Temas',
-                    'entity' : 'Organismos'
+                    'provincias' : 'Provincias',
+                    'beneficiarios' : 'Beneficiarios',
+                    'mecanismos' : 'Mecanismos'
                 },
                 resultSelector   : '#results',
                 facetSelector    : '#facets',
@@ -67,22 +88,28 @@ var App = function App() {
 
     this.addItem = function(row) {
         var self = this;
+        console.log(row);        //TODO(gb): Remove trace!!!
         this.items.push({
-            'provinces' : row[0].trim().indexOf('Todo') == 0 ? self.allProvinces : row[0].trim().split(', '),    //TODO(gb): Fix this
-            'beneficiaries' : row[1].split(', '),
-            'mecanisms' : row[2].split(', '),
-            'topics' : row[3].split(', '),
-            'title' : row[4],
-            'details' : row[5],
-            'objectives' : row[6],
-            'benefits' : row[7],
-            'amount' : row[8],
-            'web' : row[9],
-            'area' : row[10],
-            'entity' : row[11],
-            'phone' : row[12],
-            'startDate' : row[13],
-            'endDate' : row[14].substr(0,10)
+            'fondo': row[0],
+            'provincias': row[1].trim().indexOf('Todo el país') == 0 ? self.allProvinces : row[1].trim().split(', '),
+            'beneficiarios': row[2].split(', '),
+            'mecanismos': row[3].split(', '),
+            'sector': row[4],
+            'fase_de_desarrollo': row[5],
+            'tipo_de_proyecto': row[6],
+            'emp_etapas': row[7].split(', '),
+            'ins_etapas': row[8].split(', '),
+            'emp_areas': row[9].split(', '),
+            'ins_areas': row[10].split(', '),
+            'tipo_de_convocatoria': row[11],
+            'beneficiarios_descripcion': row[12],
+            'beneficiarios_tipo': row[13],
+            'objetivos': row[14],
+            'tipo_de_financiamiento': row[15],
+            'monto_maximo': row[16],
+            'cobertura_maxima': row[17],
+            'plazo_max': row[18],
+            'detalle': row[19]
         });
     }
 }
